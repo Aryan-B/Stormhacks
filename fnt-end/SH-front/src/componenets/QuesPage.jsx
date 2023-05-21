e6df83f61126beb7cc5edad37f5f036b43cc17a4
debugx-x <vaibhavs@sfu.ca>
2023-05-20 17:51:03 -0700

    add frontend

---
+++ fnt-end/SH-front/src/componenets/QuesPage.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+
+const QuesPage = () => {
+
+    return (
+        <div>
+            <h1>Ques Page</h1>
+        </div>
+    );
+}
+
+export default QuesPage;
